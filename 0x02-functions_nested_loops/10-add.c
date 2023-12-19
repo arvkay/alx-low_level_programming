7e97d95af8ebd05cb6cb4ced17f519efb5db000b
Arvind Kishore <ka630207@gmail.com>
2023-12-19 11:31:58 -0800

    Nested Loops: Temp commit file 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,20 +1,16 @@
 #include "main.h"
 
 /**
- * print_alphabet - Entry point
- * Return:0
+ * add - adds two integers and returns the result
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: sum of a and b
  */
-
-void print_alphabet(void)
+int add(int a, int b)
 {
-	char c = 'a';
+	int c;
 
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
+	c = a + b;
 
-	}
-	_putchar('\n');
-
+	return (c);
 }
